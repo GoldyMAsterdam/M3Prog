28df5893120a4702c807d768aff887f3799e41f0
Tycho Boom <38436@ma-web.nl>
2025-02-10 12:57:47 +0100

    Finished up things, starting 01

---
+++ m3prog_project/public/01/its-me.php
@@ -0,0 +1,2 @@
+<?php echo "Favoriete Serie: MrRobot, ", "Favoriete Game: Fortnite, ", "Klas: SD1C";
+?>
---
+++ m3prog_project/public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo "Titel"  ?></h1>
+    <p><?php echo "Paragraaf" ?></p>
+    <img src="images/gakster.webp" alt="Kat">
+    <section><?php echo "Section" ?> </section>
+</body>
+</html>
