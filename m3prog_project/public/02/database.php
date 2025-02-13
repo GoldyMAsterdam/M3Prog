008683eff8a7d63384e50d50b54a93ec5bd6bd69
Tycho Boom <38436@ma-web.nl>
2025-02-13 12:25:48 +0100

    Finished 01 completely and working on 02. Finished first assignment of 02

---
+++ m3prog_project/public/02/database.php
@@ -0,0 +1,9 @@
+<?php
+$huisnummer = 22;
+$rijbewijsgeslaagd = "Niet geslaagd";
+$prijsliter = "€ 95,-";
+
+echo "Huisnummer = $huisnummer". "<br>";
+echo "Geslaagd voor rijbewijs = $rijbewijsgeslaagd". "<br>";
+echo "Prijs per liter = $prijsliter". "<br>";
+?>
