f5a55eb17b4e98a84cf501b80462f3bb6168dbcb
Tycho Boom <38436@ma-web.nl>
2025-02-13 12:15:39 +0100

    Finished 01 except for showtypes.php since instructions in 07 are nonexistent

---
+++ m3prog_project/public/01/variabelennaam.php
@@ -0,0 +1,23 @@
+<?php 
+$auto2 = false; 
+$_naam = "Lucas";
+$autos = "3";
+$auto = "BMW M4";
+$$auto = 3;
+$huis = false;
+$naw = "LTF";
+// $_22 = "wallet"
+$kleurVanAuto = "Matte Black";
+$softwaredev = false;
+
+echo $auto2. "<br>";
+echo $_naam. "<br>";
+echo $autos. "<br>";
+echo $auto. "<br>";
+echo $huis. "<br>";
+echo $naw. "<br>";
+// echo intval($22). "<br>";
+echo $$auto. "<br>";
+echo $kleurVanAuto. "<br>";
+echo $softwaredev. "<br>";
+?>
