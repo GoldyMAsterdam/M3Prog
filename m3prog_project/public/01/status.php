1ace9224b20edbf7881cf676de241c283c095791
Tycho Boom <38436@ma-web.nl>
2025-02-13 11:51:18 +0100

    Done till 01/05

---
+++ m3prog_project/public/01/status.php
@@ -0,0 +1,41 @@
+<?php 
+$status = "offline". "<br>";
+$statusa = "available". "<br>";
+$statusb = "busy". "<br>";
+$statusd = "do not disturb". "<br>";
+$statuso = "offline". "<br>";
+
+
+$titelprijs = "Prijs". "<br>";
+$prijs = "€ 10,-". "<br>";
+$prijsa = "€ 20,-". "<br>";
+$prijsb = "€ 30,-". "<br>";
+$prijsd = "€ 40,-". "<br>";
+$prijso = "€ 50,-". "<br>";
+
+echo $status;
+echo $statusa;
+echo $statusb;
+echo $statusd;
+echo $statuso;
+
+echo $titelprijs;
+echo $prijs;
+echo $prijsa;
+echo $prijsb;
+echo $prijsd;
+echo $prijso;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
