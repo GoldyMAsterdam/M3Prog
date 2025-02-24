12b0a8a4ff58fb02fdd04e2aa975b4aa61414b9a
Tycho Boom <38436@ma-web.nl>
2025-02-24 13:00:38 +0100

    Finished 02ifelse for lesson 03

--- m3prog_project/public/03/if.php
+++ m3prog_project/public/03/if.php
@@ -1,9 +1,21 @@
 <?php 
+// 01
 $eenBoolVariable = false;
 $aantalopvoorraad = 20;
-$opvoorraad = $aantalopvoorraad > 0;
 
-if($opvoorraad == false) {
+// if($opvoorraad == false) {
+if($opvoorraad = $aantalopvoorraad > 20) {
     echo "Dit product is niet op voorraad";
+}
+else {
+    echo "Dit product is wel op voorraad";
+}
+if(false)//() komt uit op false
+{
+    // dit wordt NIET uitgevoerd
+}
+else
+{
+    //nu gaat deze code uitgevoerd worden
 }
 ?>
