eca0003fe11449329f815279b1e841789d28fee9
Tycho Boom <38436@ma-web.nl>
2025-02-24 12:34:05 +0100

    Finished 05 of 02 html in php

---
+++ m3prog_project/public/02/htmlinphp/menu.php
@@ -0,0 +1,19 @@
+<?php 
+$TYPEGERECHT = "Hoofdgerecht";
+$NAAMGERECHT = "Macaroni";
+$PRIJS = "20,50";
+$BESCHRIJVING = "Super duper lekker";
+$INGREDIENTENLIJST = "Macaroni en een beetje crack";
+$ALLERGENENLIJST = "Jup";
+
+ echo  "Gerecht type: $TYPEGERECHT <br>
+  <br>
+  $NAAMGERECHT €$PRIJS<br>
+  <br>
+  $BESCHRIJVING<br>
+  Ingegredientenlijst:<br>
+  $INGREDIENTENLIJST<br>
+  <br>
+  Bent u allergisch? bekijk dan hieronder de allergenen:<br>
+  $ALLERGENENLIJST<br>";
+?>
