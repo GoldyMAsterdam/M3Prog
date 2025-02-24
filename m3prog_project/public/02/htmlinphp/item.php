5f91ac94edc7c6193b0e6722a30c3ec2df3a8e89
Tycho Boom <38436@ma-web.nl>
2025-02-24 12:44:28 +0100

    Finished 02 Html in php

---
+++ m3prog_project/public/02/htmlinphp/item.php
@@ -0,0 +1,25 @@
+<?php
+    $item = "LIGHTSABRE";
+    $mod1 = "+150-200% Enhanced Damage";
+    $mod2 = "Adds 10-30 Damage";
+    $mod3 = "Adds 60-120 Magic Damage";
+    $mod4 = "Adds 1-200 Lightning Damage";
+    $mod5 = "5-7% Mana Stolen Per Hit";
+    $mod6 = "5% Chance To Cast Level 14-20 Chain Lightning On Attack";
+    $mod7 = "20% Increased Attack Speed";
+    $mod8 = "Ignore Target's Defense";
+    $mod9 = "Lightning Absorb 25%";
+    $mod10 = "+7 To Light Radius";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Item</title>
+</head>
+<body>
+    <h1><?= $item ?> </h1>
+    <p><?= "$mod1" ."<br>" ."$mod2" ."<br>" ."$mod3" ."<br>" ."$mod4" ."<br>" ."$mod5" ."<br>" ."$mod6" ."<br>" ."$mod7" ."<br>" ."<br>" ."$mod8" ."<br>" ."$mod9" ."<br>" ."$mod10" ?> </p>
+</body>
+</html>
