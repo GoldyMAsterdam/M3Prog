20fc6821e884052130bc03874d984e6c2bb869fb
Tycho Boom <38436@ma-web.nl>
2025-02-24 11:56:20 +0100

    Added 02 in files and finished 01 of html in php

---
+++ m3prog_project/public/02/htmlinphp/plak.php
@@ -0,0 +1,21 @@
+<?php 
+
+$standaardString = 'hello world<br>';
+$extraString = 'extra world<br>'; 
+
+$totaleString = "hello" ." ". "world" ."<br>";
+
+$Voornaam = "Luigi";
+$Achternaam = "Mario";
+$fullName = $Voornaam ." ". $Achternaam ."<br>";
+
+echo $fullName; 
+
+$tekst = "$Voornaam kocht 1 appel". "<br>";
+
+echo $tekst;
+
+
+echo $standaardString;
+echo $extraString;
+?>
