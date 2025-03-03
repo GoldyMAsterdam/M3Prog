8dd7671e08ffe3cb487e2c7be81d72d4fe13e70c
Tycho Boom <38436@ma-web.nl>
2025-03-03 11:49:05 +0100

    Finished 02arrays_index of 04

---
+++ m3prog_project/public/04/arrays_uitleg.php
@@ -0,0 +1,21 @@
+<?php
+// 01 
+$nummers = [1,2,3,4,5,6,7,8,9,10];
+print_r($nummers);
+
+// 02 
+$dagen = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"];
+echo $dagen[4] ."<br>";
+echo $dagen[2] ."<br>";
+echo $dagen[6] ."<br>";
+echo $dagen[1] ."<br>";
+
+echo "<br>";
+echo $nummers[4];
+echo "<br>";
+echo "<br>";
+echo $nummers[1];
+echo "<br>";
+echo "<br>";
+echo $nummers[9];
+echo "<br>";
