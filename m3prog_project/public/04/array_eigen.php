91317665401fda6f4fb2e101c84b11df9affeb92
Tycho Boom <38436@ma-web.nl>
2025-03-03 12:04:34 +0100

    Finished 03arrays_eigen_array of 04arrays

---
+++ m3prog_project/public/04/array_eigen.php
@@ -0,0 +1,19 @@
+<?php 
+$games = ["Fortnite", "Hypixel Skyblock", "Minecraft", "Warframe", "Rainbow Six Siege"];
+
+echo "<pre>";
+print_r($games);
+echo "</pre>";
+
+echo "<pre>";
+var_dump($games);
+echo "</pre>";
+
+echo "<pre>";
+echo $games[2];
+echo "</pre>";
+
+echo "<pre>";
+echo $games[4];
+echo "</pre>";
+?>
