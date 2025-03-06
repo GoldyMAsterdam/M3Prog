ce25c9eed10225cc8e75c899c958eebfa782b29a
Tycho Boom <38436@ma-web.nl>
2025-03-06 12:46:10 +0100

    Finished 01 of 05 Functions

--- m3prog_project/public/05/phpfunctions.php
+++ m3prog_project/public/05/phpfunctions.php
@@ -28,3 +28,30 @@ print($woordlengte2);
  /* vraag 4)
     hoevaak heb je nu de function strlen aangeroepen?
 Antwoord: 2 keer
+*/
+echo "<br>";
+$tweeMacht2 = pow(2,2);
+$tweeMacht10 = pow(2,10);
+print($tweeMacht10);
+
+$derdemacht = pow(3,4);
+echo "<br>";
+print($derdemacht);
+
+/*
+vraag 1)
+hoe geven we het resultaat door?
+
+Antwoord: met een print statement
+
+*/
+
+$Naam = "Wario";
+$NaamGroot = strtoupper($Naam);
+echo "<br>";
+print($NaamGroot);
+
+$Naam2 = "Mario";
+$NaamGroot2 = strtolower($Naam2);
+echo "<br>";
+print($NaamGroot2);
