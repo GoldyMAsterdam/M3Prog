9292c38d47025b4577fa5be4f736b96cd9b0d239
Tycho Boom <38436@ma-web.nl>
2025-03-06 12:38:01 +0100

    Done with phpfunctions 05

---
+++ m3prog_project/public/05/phpfunctions.php
@@ -0,0 +1,30 @@
+<?php 
+$hoelangisditwoord = "scrabbleWoord";
+$woordlengte = strlen($hoelangisditwoord);
+
+print($woordlengte);
+echo "<br>";
+
+/*vraag 1)
+
+    wat is het type het argument van strlen?
+Antwoord: string
+
+vraag 2)
+    wat is het type van wat strlen returned?
+Antwoord: een int
+vraag 3)
+
+    het getal wat strlen returned, wat is dat?
+Antwoord: de lengte van de string
+
+*/
+
+$nogeenlangwoord = "Hoiditiseenerglangwoordmetsuperveelkaraktersikwilditsuperlangmakenwantwaaromniet";
+$woordlengte2 = strlen($nogeenlangwoord);
+
+print($woordlengte2);
+
+ /* vraag 4)
+    hoevaak heb je nu de function strlen aangeroepen?
+Antwoord: 2 keer
