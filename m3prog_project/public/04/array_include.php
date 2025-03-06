50af924baa2c25887e473c148c93537fee44fa1b
Tycho Boom <38436@ma-web.nl>
2025-03-06 12:25:13 +0100

    Finished 04

--- m3prog_project/public/04/array_include.php
+++ m3prog_project/public/04/array_include.php
@@ -1,43 +1,35 @@
 <?php 
    $reviews = 
-   [
-       ["demon's souls",10],
-       ["mario wonder",9.5],
-       ["mega man 2",8],
-   ];
+[
+    ["demon's souls", 10, "L Game"], 
+    ["mario wonder", 9.5, "L Game"], 
+    ["mega man 2", 8, "L Game"]
+];
 
-   $announcements = [
+   $announcements = 
+   [
         "a.html",
         "b.html",
         "c.html",
         "d.html",
         "e.html"
-    ]
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Array include</title>
-</head>
-<body>
-    <?php
+   ];
 
-    for ($i = 0; $i < count($reviews); $i++) {
+
+for ($i = 0; $i < count($reviews); $i++) {
     $review = $reviews[$i];
-
     $title = $review[0];
     $cijfer = $review[1];
-    }
+    $conclusion = $review[2];
+    echo "<h2>" .$title. "</h2>";
+    echo "<h4>" .$cijfer. "</h4>";
+    echo "<p>" .$conclusion. "</p>";
+}
 
-    echo $title. "<br>";
-    echo $cijfer. "<br>";
-
-    for ($i = 0; $i < count($announcements); $i++) { 
+   for ($i = 0; $i < count($announcements); $i++) { 
         $announcement = $announcements[$i];
         include "announcements/$announcement";
     }
-    ?>
-</body>
-</html>
+    
+?>
+    
