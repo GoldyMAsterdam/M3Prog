5f8e620976a7ec7d8840a21158f6091885f4b7fa
Tycho Boom <38436@ma-web.nl>
2025-03-10 13:04:33 +0100

    Done with 03 functionReturn

---
+++ m3prog_project/public/05/returncalc.php
@@ -0,0 +1,30 @@
+<?php
+
+
+function add($getal1, $getal2) {
+    $som = $getal1 + $getal2; 
+    return $som;
+}
+
+function subtract($getal1, $getal2) {
+    $som2 = $getal1 - $getal2; 
+    return $som2;
+}
+
+function divide($getal1, $getal2) {
+    $som3 = $getal1 / $getal2; 
+    return $som3;
+}
+
+function multiply($getal1, $getal2) {
+    $som4 = $getal1 * $getal2; 
+    return $som4;
+}
+
+$getal1 = 100;
+$getal2 = 100;
+
+echo add($getal1, $getal2). "<br>";
+echo subtract($getal1, $getal2). "<br>";
+echo multiply($getal1, $getal2). "<br>";
+
