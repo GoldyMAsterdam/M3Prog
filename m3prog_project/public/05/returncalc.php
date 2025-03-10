6e0a601bb5f9d5439420404490ae68201d99187a
Tycho Boom <38436@ma-web.nl>
2025-03-10 13:08:16 +0100

    Done with 08 voorbereiding of 05 functions

--- m3prog_project/public/05/returncalc.php
+++ m3prog_project/public/05/returncalc.php
@@ -3,22 +3,22 @@
 
 function add($getal1, $getal2) {
     $som = $getal1 + $getal2; 
-    return $som;
+    // return $som;
 }
 
 function subtract($getal1, $getal2) {
     $som2 = $getal1 - $getal2; 
-    return $som2;
+    // return $som2;
 }
 
 function divide($getal1, $getal2) {
     $som3 = $getal1 / $getal2; 
-    return $som3;
+    // return $som3;
 }
 
 function multiply($getal1, $getal2) {
     $som4 = $getal1 * $getal2; 
-    return $som4;
+    // return $som4;
 }
 
 $getal1 = 100;
@@ -26,5 +26,7 @@ $getal2 = 100;
 
 echo add($getal1, $getal2). "<br>";
 echo subtract($getal1, $getal2). "<br>";
+echo divide($getal1, $getal2). "<br>";
 echo multiply($getal1, $getal2). "<br>";
+
 
