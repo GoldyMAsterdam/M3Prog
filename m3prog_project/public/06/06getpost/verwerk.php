4866de0e23865a15fcc56f0c84f587e9c65abdef
Tycho Boom <38436@ma-web.nl>
2025-03-27 12:22:34 +0100

    Finetuned last assignment 06getpost

--- m3prog_project/public/06/06getpost/verwerk.php
+++ m3prog_project/public/06/06getpost/verwerk.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "<h1>Het Weer!</h1>";
+
 echo "<p>Stad: " . $_POST['City'] . "</p>"; 
 echo "<p>Datum: " . $_POST['date'] . "</p>";
 echo "<p>Temperatuur: " . $_POST['degrees'] . "</p>";
