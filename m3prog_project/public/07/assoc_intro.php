32860b9f833e409d37db5ce927bf87d60b13284e
Tycho Boom <38436@ma-web.nl>
2025-03-31 10:31:46 +0200

    Finished 01 arrays of 07

---
+++ m3prog_project/public/07/assoc_intro.php
@@ -0,0 +1,17 @@
+<?php
+$trophiesPerGame = [
+    "Demon's souls" => 11,
+    "Mario wonder" => 0,
+    "Rayman Origins" => 9,
+    "Tetris" => 2
+];
+
+$trophiesForDS = $trophiesPerGame["Demon's souls"];
+$trophiesForMW = $trophiesPerGame["Mario wonder"];
+$trophiesForRO = $trophiesPerGame["Rayman Origins"];
+$tetris = $trophiesPerGame["Tetris"];
+
+echo $trophiesForDS. "<br>";
+echo $trophiesForMW. "<br>";
+echo $trophiesForRO. "<br>";
+echo $tetris;
