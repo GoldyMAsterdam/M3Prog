4362bfc55c75768dbbc161efb98b2c698d281f72
Tycho Boom <38436@ma-web.nl>
2025-04-03 11:28:26 +0200

    Finished 07 arrays

--- m3prog_project/public/07/assocjson.php
+++ m3prog_project/public/07/assocjson.php
@@ -1,21 +1,45 @@
 <?php
 
+$verbruik = [
+    'Fohn' => 29,
+    'Koelkast' => 10,
+    'TV' => 20,
+    'Wasmachine' => 10,
+    'PC' => 20,
+];
 
+$persoon = [
+    'voornaam' => 'Luigi',
+    'achternaam' => 'Mario',
+    'leeftijd' => 19,
+    'minst favoriet eten' => 'couscous',
+    'woonplaats' => 'Heerhugowaard'
+];
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?= $json = json_encode($verbruik);
-    echo $json;
-    header("Content-Type: application/json");?>
-    <?php foreach($verbruik as $key => $value): ?>
-        <p><?php echo $key; ?> verbruikt <?php echo $value; ?> kWh</p>
-</body>
-<?php endforeach; ?>
-</html>
+$trophiesPerGame = [
+    "Demon's souls" => 11,
+    "Mario wonder" => 0,
+    "Rayman Origins" => 9,
+    "Tetris" => 2
+];
+
+$information = [
+    'maandag' => 20,
+    'dinsdag' => 14,
+    'woensdag' => 34,
+    'donderdag' => 4,
+    'vrijdag' => 41,
+    'zaterdag' => 10,
+    'zondag' => 1
+];
+
+$jsonArray = [
+    'trophiesPerGame' => $trophiesPerGame,
+    'persoon' => $persoon,
+    'weer' => $information,
+    'verbruik' => $verbruik
+];
+
+$json = json_encode($jsonArray);
+echo $json;
+header("Content-Type: application/json");
